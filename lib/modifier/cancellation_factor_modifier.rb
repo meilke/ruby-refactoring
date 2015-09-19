4be84bd84470c93f3776bc600ecbc9e6b821dfeb
Christian Meilke <cme@auctionata.com>
2015-09-19 20:23:25 +0200

    Cancellation modifier + tests

--- lib/modifier/cancellation_factor_modifier.rb
+++ lib/modifier/cancellation_factor_modifier.rb
@@ -2,14 +2,13 @@ require_relative 'base_modifier'
 
 class CancellationFactorModifier < BaseModifier
   
-  def initialize(cancellation_factor)
+  def initialize(cancellation_factor, columns)
+    super(columns)
     @cancellation_factor = cancellation_factor
   end
 
-  def modify(hash)
-    ['number of commissions'].each do |key|
-      hash[key] = (@cancellation_factor * hash[key][0].from_german_to_f).to_german_s
-    end
+  def modify_inner(hash, key)
+    hash[key] = (@cancellation_factor * hash[key][0].from_german_to_f).to_german_s
   end
 
 end
--- spec/modifier_spec.rb
+++ spec/modifier_spec.rb
@@ -57,8 +57,22 @@ describe 'Modifiers' do
       it { should eq({'Account ID' => ['0,1', '0,2', '0,3']}) }
     end
 
-    context 'does some re-formatting and in the end it is the same value (weird...)' do
+    context 'does some re-formatting of the first value and in the end it is the same value (weird...)' do
       it { should eq({'Account ID' => '0,1'}) }
+    end
+  end
+
+  context CancellationFactorModifier do
+    let(:hash) { {'Account ID' => ['0,1', '0,2', '0,3']} }
+    subject { CancellationFactorModifier.new(8, columns).modify(hash) }
+  
+    context 'leaves unconfigured columns untouched' do
+      let(:columns) { ['something else'] }
+      it { should eq({'Account ID' => ['0,1', '0,2', '0,3']}) }
+    end
+
+    context 'does some re-formatting of a multiplied first value and in the end it is the same value (weird...)' do
+      it { should eq({'Account ID' => '0,8'}) }
     end
   end
 
