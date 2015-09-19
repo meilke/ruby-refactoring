a843e50e8cf28917c856bd607d6803efb3139231
Christian Meilke <cme@auctionata.com>
2015-09-19 16:31:06 +0200

    Combiner tests: Replaced deprecated method usage

--- spec/combiner_spec.rb
+++ spec/combiner_spec.rb
@@ -25,7 +25,7 @@ RSpec::Matchers.define :return_elements do |*expected|
 		read_elements = read_from_enumerator(enumerator)
 		read_elements == expected
 	end
-	failure_message_for_should do |enumerator|
+	failure_message do |enumerator|
 		"expected that #{enumerator} would return #{expected.inspect}, but it returned #{read_elements.inspect}"
 	end
 end
