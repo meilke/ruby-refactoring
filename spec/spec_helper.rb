415fba2fbb790653d756a73a90b581941fe8f926
Christian Meilke <cme@auctionata.com>
2015-09-19 17:11:09 +0200

    Combiner tests: Moved helper method to spec helper + spaces instead of tabs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,14 @@
 $:.unshift File.expand_path('../lib/', File.dirname(__FILE__))
 require 'rspec'
+
+def read_from_enumerator(enumerator)
+  result = []
+  loop do
+    begin
+      result << enumerator.next
+    rescue StopIteration
+      break
+    end
+  end
+  result
+end
