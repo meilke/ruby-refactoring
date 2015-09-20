f956e20863d93c9a8216d6a3c5fcfef02836a3c8
Christian Meilke <cme@auctionata.com>
2015-09-20 16:48:27 +0200

    Base script with spaces instead of tabs

--- modifier.rb
+++ modifier.rb
@@ -21,97 +21,97 @@ end
 
 class Modifier
 
-	KEYWORD_UNIQUE_ID = 'Keyword Unique ID'
-	LAST_VALUE_WINS = ['Account ID', 'Account Name', 'Campaign', 'Ad Group', 'Keyword', 'Keyword Type', 'Subid', 'Paused', 'Max CPC', 'Keyword Unique ID', 'ACCOUNT', 'CAMPAIGN', 'BRAND', 'BRAND+CATEGORY', 'ADGROUP', 'KEYWORD']
-	LAST_REAL_VALUE_WINS = ['Last Avg CPC', 'Last Avg Pos']
-	INT_VALUES = ['Clicks', 'Impressions', 'ACCOUNT - Clicks', 'CAMPAIGN - Clicks', 'BRAND - Clicks', 'BRAND+CATEGORY - Clicks', 'ADGROUP - Clicks', 'KEYWORD - Clicks']
-	FLOAT_VALUES = ['Avg CPC', 'CTR', 'Est EPC', 'newBid', 'Costs', 'Avg Pos']
-	CANCELLATION_VALUES = ['number of commissions']
-	CANCELLATION_SALE_AMOUNT_VALUES = ['Commission Value', 'ACCOUNT - Commission Value', 'CAMPAIGN - Commission Value', 'BRAND - Commission Value', 'BRAND+CATEGORY - Commission Value', 'ADGROUP - Commission Value', 'KEYWORD - Commission Value']
+  KEYWORD_UNIQUE_ID = 'Keyword Unique ID'
+  LAST_VALUE_WINS = ['Account ID', 'Account Name', 'Campaign', 'Ad Group', 'Keyword', 'Keyword Type', 'Subid', 'Paused', 'Max CPC', 'Keyword Unique ID', 'ACCOUNT', 'CAMPAIGN', 'BRAND', 'BRAND+CATEGORY', 'ADGROUP', 'KEYWORD']
+  LAST_REAL_VALUE_WINS = ['Last Avg CPC', 'Last Avg Pos']
+  INT_VALUES = ['Clicks', 'Impressions', 'ACCOUNT - Clicks', 'CAMPAIGN - Clicks', 'BRAND - Clicks', 'BRAND+CATEGORY - Clicks', 'ADGROUP - Clicks', 'KEYWORD - Clicks']
+  FLOAT_VALUES = ['Avg CPC', 'CTR', 'Est EPC', 'newBid', 'Costs', 'Avg Pos']
+  CANCELLATION_VALUES = ['number of commissions']
+  CANCELLATION_SALE_AMOUNT_VALUES = ['Commission Value', 'ACCOUNT - Commission Value', 'CAMPAIGN - Commission Value', 'BRAND - Commission Value', 'BRAND+CATEGORY - Commission Value', 'ADGROUP - Commission Value', 'KEYWORD - Commission Value']
 
   LINES_PER_FILE = 120000
 
-	def initialize(saleamount_factor, cancellation_factor)
-		@saleamount_factor = saleamount_factor
-		@cancellation_factor = cancellation_factor
-	end
+  def initialize(saleamount_factor, cancellation_factor)
+    @saleamount_factor = saleamount_factor
+    @cancellation_factor = cancellation_factor
+  end
 
-	def modify(output, input)
-		input = sort(input)
-		input_enumerator = lazy_read(input)
+  def modify(output, input)
+    input = sort(input)
+    input_enumerator = lazy_read(input)
 
-		modifiers = [
-			LastValueWinsModifier.new(LAST_VALUE_WINS),
-			LastRealValueWinsModifier.new(LAST_REAL_VALUE_WINS),
-			IntValuesModifier.new(INT_VALUES),
-			FloatValueModifier.new(FLOAT_VALUES),
-			CancellationFactorModifier.new(@cancellation_factor, CANCELLATION_VALUES),
-			CancellationSaleAmountFactorModifier.new(@cancellation_factor, @saleamount_factor, CANCELLATION_SALE_AMOUNT_VALUES)
-		]
+    modifiers = [
+      LastValueWinsModifier.new(LAST_VALUE_WINS),
+      LastRealValueWinsModifier.new(LAST_REAL_VALUE_WINS),
+      IntValuesModifier.new(INT_VALUES),
+      FloatValueModifier.new(FLOAT_VALUES),
+      CancellationFactorModifier.new(@cancellation_factor, CANCELLATION_VALUES),
+      CancellationSaleAmountFactorModifier.new(@cancellation_factor, @saleamount_factor, CANCELLATION_SALE_AMOUNT_VALUES)
+    ]
 
-		merger = MergeAndCombine.new(KEYWORD_UNIQUE_ID, modifiers).process(input_enumerator)
+    merger = MergeAndCombine.new(KEYWORD_UNIQUE_ID, modifiers).process(input_enumerator)
 
     done = false
     file_index = 0
     file_name = output.gsub('.txt', '')
     while not done do
-		  CSV.open(file_name + "_#{file_index}.txt", "wb", { :col_sep => "\t", :headers => :first_row, :row_sep => "\r\n" }) do |csv|
-			  headers_written = false
+      CSV.open(file_name + "_#{file_index}.txt", "wb", { :col_sep => "\t", :headers => :first_row, :row_sep => "\r\n" }) do |csv|
+        headers_written = false
         line_count = 0
-			  while line_count < LINES_PER_FILE
-				  begin
-					  merged = merger.next
-					  if not headers_written
-						  csv << merged.keys
-						  headers_written = true
+        while line_count < LINES_PER_FILE
+          begin
+            merged = merger.next
+            if not headers_written
+              csv << merged.keys
+              headers_written = true
               line_count +=1
-					  end
-					  csv << merged
+            end
+            csv << merged
             line_count +=1
-				  rescue StopIteration
+          rescue StopIteration
             done = true
-					  break
-				  end
-			  end
+            break
+          end
+        end
         file_index += 1
-		  end
+      end
     end
-	end
+  end
 
-	private
+  private
 
-	DEFAULT_CSV_OPTIONS = { :col_sep => "\t", :headers => :first_row }
+  DEFAULT_CSV_OPTIONS = { :col_sep => "\t", :headers => :first_row }
 
-	def parse(file)
-		CSV.read(file, DEFAULT_CSV_OPTIONS)
-	end
+  def parse(file)
+    CSV.read(file, DEFAULT_CSV_OPTIONS)
+  end
 
-	def lazy_read(file)
-		Enumerator.new do |yielder|
-			CSV.foreach(file, DEFAULT_CSV_OPTIONS) do |row|
-				yielder.yield(row)
-			end
-		end
-	end
+  def lazy_read(file)
+    Enumerator.new do |yielder|
+      CSV.foreach(file, DEFAULT_CSV_OPTIONS) do |row|
+        yielder.yield(row)
+      end
+    end
+  end
 
-	def write(content, headers, output)
-		CSV.open(output, "wb", { :col_sep => "\t", :headers => :first_row, :row_sep => "\r\n" }) do |csv|
-			csv << headers
-			content.each do |row|
-				csv << row
-			end
-		end
-	end
+  def write(content, headers, output)
+    CSV.open(output, "wb", { :col_sep => "\t", :headers => :first_row, :row_sep => "\r\n" }) do |csv|
+      csv << headers
+      content.each do |row|
+        csv << row
+      end
+    end
+  end
 
-	def sort(file)
-		output = "#{file}.sorted"
-		content_as_table = parse(file)
-		headers = content_as_table.headers
-		index_of_key = headers.index('Clicks')
-		content = content_as_table.sort_by { |a| -a[index_of_key].to_i }
-		write(content, headers, output)
-		return output
-	end
+  def sort(file)
+    output = "#{file}.sorted"
+    content_as_table = parse(file)
+    headers = content_as_table.headers
+    index_of_key = headers.index('Clicks')
+    content = content_as_table.sort_by { |a| -a[index_of_key].to_i }
+    write(content, headers, output)
+    return output
+  end
 end
 
 modified = input = latest('project_2012-07-27_2012-10-10_performancedata')
