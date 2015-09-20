dd1d40416358c2ff59ff0475b857215e0d8a1a0b
Christian Meilke <cme@auctionata.com>
2015-09-20 20:19:53 +0200

    Coverage using code climate

---
+++ Gemfile
@@ -0,0 +1,3 @@
+source 'https://rubygems.org'
+gem "rspec"
+gem "codeclimate-test-reporter", group: :test, require: nil
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
 $:.unshift File.expand_path('../lib/', File.dirname(__FILE__))
 require 'rspec'
+require 'codeclimate-test-reporter'
+
+CodeClimate::TestReporter.start
 
 def read_from_enumerator(enumerator)
   result = []
