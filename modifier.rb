033319cc86efd9bb680ddf930b65c1d762e867fd
Christian Meilke <cme@auctionata.com>
2015-09-20 16:47:45 +0200

    Moved new component into base script

--- modifier.rb
+++ modifier.rb
@@ -1,4 +1,5 @@
-require File.expand_path('lib/combiner',File.dirname(__FILE__))
+require_relative 'lib/merge_and_combine'
+Dir['./lib/modifier/*.rb'].each {|file| require file }
 require 'csv'
 require 'date'
 
@@ -18,18 +19,6 @@ def latest(name)
   files.last
 end
 
-class String
-	def from_german_to_f
-		self.gsub(',', '.').to_f
-	end
-end
-
-class Float
-	def to_german_s
-		self.to_s.gsub('.', ',')
-	end
-end
-
 class Modifier
 
 	KEYWORD_UNIQUE_ID = 'Keyword Unique ID'
@@ -37,6 +26,8 @@ class Modifier
 	LAST_REAL_VALUE_WINS = ['Last Avg CPC', 'Last Avg Pos']
 	INT_VALUES = ['Clicks', 'Impressions', 'ACCOUNT - Clicks', 'CAMPAIGN - Clicks', 'BRAND - Clicks', 'BRAND+CATEGORY - Clicks', 'ADGROUP - Clicks', 'KEYWORD - Clicks']
 	FLOAT_VALUES = ['Avg CPC', 'CTR', 'Est EPC', 'newBid', 'Costs', 'Avg Pos']
+	CANCELLATION_VALUES = ['number of commissions']
+	CANCELLATION_SALE_AMOUNT_VALUES = ['Commission Value', 'ACCOUNT - Commission Value', 'CAMPAIGN - Commission Value', 'BRAND - Commission Value', 'BRAND+CATEGORY - Commission Value', 'ADGROUP - Commission Value', 'KEYWORD - Commission Value']
 
   LINES_PER_FILE = 120000
 
@@ -47,24 +38,18 @@ class Modifier
 
 	def modify(output, input)
 		input = sort(input)
-
 		input_enumerator = lazy_read(input)
 
-		combiner = Combiner.new do |value|
-			value[KEYWORD_UNIQUE_ID]
-		end.combine(input_enumerator)
+		modifiers = [
+			LastValueWinsModifier.new(LAST_VALUE_WINS),
+			LastRealValueWinsModifier.new(LAST_REAL_VALUE_WINS),
+			IntValuesModifier.new(INT_VALUES),
+			FloatValueModifier.new(FLOAT_VALUES),
+			CancellationFactorModifier.new(@cancellation_factor, CANCELLATION_VALUES),
+			CancellationSaleAmountFactorModifier.new(@cancellation_factor, @saleamount_factor, CANCELLATION_SALE_AMOUNT_VALUES)
+		]
 
-		merger = Enumerator.new do |yielder|
-			while true
-				begin
-					list_of_rows = combiner.next
-					merged = combine_hashes(list_of_rows)
-					yielder.yield(combine_values(merged))
-				rescue StopIteration
-					break
-				end
-			end
-		end
+		merger = MergeAndCombine.new(KEYWORD_UNIQUE_ID, modifiers).process(input_enumerator)
 
     done = false
     file_index = 0
@@ -95,54 +80,6 @@ class Modifier
 
 	private
 
-	def combine(merged)
-		result = []
-		merged.each do |_, hash|
-			result << combine_values(hash)
-		end
-		result
-	end
-
-	def combine_values(hash)
-		LAST_VALUE_WINS.each do |key|
-			hash[key] = hash[key].last
-		end
-		LAST_REAL_VALUE_WINS.each do |key|
-			hash[key] = hash[key].select {|v| not (v.nil? or v == 0 or v == '0' or v == '')}.last
-		end
-		INT_VALUES.each do |key|
-			hash[key] = hash[key][0].to_s
-		end
-		FLOAT_VALUES.each do |key|
-			hash[key] = hash[key][0].from_german_to_f.to_german_s
-		end
-		['number of commissions'].each do |key|
-			hash[key] = (@cancellation_factor * hash[key][0].from_german_to_f).to_german_s
-		end
-		['Commission Value', 'ACCOUNT - Commission Value', 'CAMPAIGN - Commission Value', 'BRAND - Commission Value', 'BRAND+CATEGORY - Commission Value', 'ADGROUP - Commission Value', 'KEYWORD - Commission Value'].each do |key|
-			hash[key] = (@cancellation_factor * @saleamount_factor * hash[key][0].from_german_to_f).to_german_s
-		end
-		hash
-	end
-
-	def combine_hashes(list_of_rows)
-		keys = []
-		list_of_rows.each do |row|
-			next if row.nil?
-			row.headers.each do |key|
-				keys << key
-			end
-		end
-		result = {}
-		keys.each do |key|
-			result[key] = []
-			list_of_rows.each do |row|
-				result[key] << (row.nil? ? nil : row[key])
-			end
-		end
-		result
-	end
-
 	DEFAULT_CSV_OPTIONS = { :col_sep => "\t", :headers => :first_row }
 
 	def parse(file)
@@ -166,7 +103,6 @@ class Modifier
 		end
 	end
 
-	public
 	def sort(file)
 		output = "#{file}.sorted"
 		content_as_table = parse(file)
